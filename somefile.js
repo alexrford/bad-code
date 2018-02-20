6a39ca89bd5500bd6e11ec01f927e03c46d5dd57
alexf-test <33869700+alexf-test@users.noreply.github.com>
2018-02-20 11:18:39 +0000

    Update somefile.js

--- somefile.js
+++ somefile.js
@@ -1,5 +1,10 @@
-alert('hello');
-
-function foo(x) {
-  return x;
+function foo(x, y) {
+  return x + x;
 }
+
+
+alert(foo(7));
+
+var capacity = 1;
+capacity = 2;
+capacity = capacity; var a = new Array(capacity + capacity>>1);
