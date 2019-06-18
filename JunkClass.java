b4c53d90da67e9f751c72d4ce8a8edba62eed900
Alex Ford <alexf@semmle.com>
2019-06-18 12:56:30 +0100

    alert

--- JunkClass.java
+++ JunkClass.java
@@ -4,9 +4,45 @@ public class JunkClass {
 
 	public static void main(String [] args) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		/* some leading context here */ System.out.println("world"); /* more words */ /*  etc */  /* more words */ /*  etc */  /* more words */ /*  etc */  /* more words */ /*  etc */  String.format("Ambiguous values of metric '%s' for file path '%s' %s",
-
-
 
 
 
